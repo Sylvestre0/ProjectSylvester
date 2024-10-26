f5b36de930be8c3770378b7204c6883497923f3f
sylvester0 <rafael.oliveira.59522@a.fecaf.com.br>
2024-10-26 20:29:06 -0300

    fix: pequeno erro no err: any

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -18,7 +18,7 @@ export const addUser = async (req: Request, res: Response) => {
   try {
     const user = await userRepository.addUser(name, email);
     res.status(201).json(user);
-  } catch (err) {
+  } catch (err: any) {
     console.error(err);
     res.status(500).json({ error: 'Erro ao adicionar usuário' });
   }
