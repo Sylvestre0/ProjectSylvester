127f2198080ba3211a28a031f7de061b778238ff
sylvester0 <rafael.oliveira.59522@a.fecaf.com.br>
2024-10-26 01:10:46 -0300

    add/rename: adicionado esquema de bancos e renome de userController.ts -> userModel.ts

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,28 +1,12 @@
-import {Dialect, Sequelize} from "sequelize";
-import mysql2 from "mysql2/promise";
+import { Pool } from 'pg';
 
-const dbconfig={
-    database: "loja1",
-    username: "root",
-    password: "root2",
-    options:{
-        host: "localhost",
-        dialect: "mysql" as Dialect,
-        logging: false,
-    }
-};
-
-async function createDataBase() {
-    const conexao = await mysql2.createConnection({
-    host: dbconfig.options.host,
-    user: dbconfig.username,
-    password: dbconfig.password,
+// Substitua pela sua string de conexão do Render.com
+const connectionString = "postgresql://postgres:ZyLQtyLLmeVNmIcIxZWWQiLafdOjoSHz@autorack.proxy.rlwy.net:38056/railway";
+const pool = new Pool({
+  connectionString,
+  ssl: {
+    rejectUnauthorized: false,
+  }
 });
 
-await conexao.query("Create DataBase if not exists \'${dbconfig.database}\';");
-await conexao.end();
-}
-
-const sequelize = new Sequelize(dbconfig.database,dbconfig.username,dbconfig.password,dbconfig.options);
-export default sequelize;
-export {createDataBase};
+export default pool;
