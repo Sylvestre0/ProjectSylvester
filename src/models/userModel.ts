516af2007863cd680b9c5bdf66834c332bf3dff7
sylvester0 <rafael.oliveira.59522@a.fecaf.com.br>
2024-10-26 01:12:26 -0300

    add: adicionado nova URL do RailWay

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,33 +1,5 @@
-import { DataType,DataTypes,Model } from "sequelize";
-import sequelize from "../config/config";
-
-class User extends Model {
-    public id!: number;
-    public gmail!: string;
-    public nome!: string;
-    public cpf!: number;
-    public readonly createdAt!: Date;
-    public readonly updatedAt!: Date;
-}
-
-User.init({
-    id:{
-        type: DataTypes.INTEGER.UNSIGNED,
-        autoIncrement:true,
-        primaryKey: true,
-    },
-    gmail:{
-        type:DataTypes.STRING(100),
-        allowNull: false,  
-    },
-    nome:{
-        type:DataTypes.STRING(100),
-        allowNull: false,
-    },
-    cpf:{
-        type: DataTypes.STRING(15),
-        allowNull: false, 
-    }
-},{
-sequelize,tableName:"nome_da_table",
-})
+export interface User {
+    id: number;
+    name: string;
+    email: string;
+  }
