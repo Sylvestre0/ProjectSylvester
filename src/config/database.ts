fba14d89547370719bc0f2da5d74188c8b250861
Sylvestre0 <rafael.oliveira.59522@a.fecaf.com.br>
2024-10-28 20:16:23 +0000

    update: adiciona uma linha que permite abrir arquivos estaticos

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,11 +2,11 @@ import { Pool } from 'pg';
 
 // Substitua pela sua string de conexão do Render.com
 const connectionString = "postgresql://postgres:ZyLQtyLLmeVNmIcIxZWWQiLafdOjoSHz@autorack.proxy.rlwy.net:38056/railway";
-const pool = new Pool({
+const database = new Pool({
   connectionString,
   ssl: {
     rejectUnauthorized: false,
   }
 });
 
-export default pool;
+export default database;
