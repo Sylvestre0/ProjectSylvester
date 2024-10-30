9464761250044c62ca7e7418e4f010fe4983aad6
Sylvestre0 <rafael.oliveira.59522@a.fecaf.com.br>
2024-10-30 19:11:06 +0000

    fix: consertado nomeclatura de variavel que tinha letras maiusculas problematicas do camelcase

--- src/migrations/migrations.ts
+++ src/migrations/migrations.ts
@@ -8,8 +8,8 @@ const createUsersTable = async () => {
         id SERIAL PRIMARY KEY,
         name VARCHAR(100) NOT NULL,
         email VARCHAR(100) UNIQUE NOT NULL,
-        passwordHash VARCHAR(64),
-        googleId VARCHAR(255) UNIQUE
+        passwordhash VARCHAR(64),
+        googleid VARCHAR(255) UNIQUE
       );
     `;
     await client.query(queryText);
